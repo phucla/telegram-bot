66323286e4ccb7ef17f9fce2fbbb86e9ce987e70
Phuc <phuc@MacBook-Pro-cua-Phuc.local>
2025-11-27 22:56:43 +0700

    Update config

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const { Telegraf } = require('telegraf');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const express = require('express')
 const { scrapeWebsite } = require('./src/scrapeWebsite')
 
 require('dotenv').config(); 
 // Use environment variables for security in a real project
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const app = express();
+const port = 3000; // Or any desired port number
+const webhookDomain = 'https://telegram-bot-7lru.onrender.com'
 
 // Function to perform web scraping
 
@@ -34,5 +38,27 @@ bot.on('text', (ctx) => {
 });
 
 // Launch the bot
-bot.launch();
-console.log('Bot is up and running...');
+// bot.launch();
+bot.launch({
+  webhook: {
+    // Public domain for webhook; e.g.: example.com
+    domain: webhookDomain,
+
+    // Port to listen on; e.g.: 8080
+    port: port,
+
+    // Optional path to listen for.
+    // `bot.secretPathComponent()` will be used by default
+    // path: webhookPath,
+
+    // Optional secret to be sent back in a header for security.
+    // e.g.: `crypto.randomBytes(64).toString("hex")`
+    // secretToken: randomAlphaNumericString,
+  },
+});
+// app.use(await bot.createWebhook({ domain: 'webhookDomain' }));
+
+bot.on("text", ctx => ctx.reply("Hello"));
+
+// app.listen(port, () => console.log("Listening on port", port));
+console.log('Bot is up and running port:', port);
