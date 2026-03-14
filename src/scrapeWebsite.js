7b6f27e5ae5c0a1d069dc442d8e0ce421cf04b06
phucla <laducphuc@gmail.com>
2026-03-15 00:49:25 +0700

    Update build

--- src/scrapeWebsite.js
+++ src/scrapeWebsite.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import * as cheerio from 'cheerio'
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 async function scrapeWebsite(url) {
     try {
