873b3f101416d650b7a92d047f4a03164d74608e
phucla <laducphuc@gmail.com>
2026-03-15 00:02:57 +0700

    test token

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const { Telegraf } = require('telegraf');
-const express = require('express')
-require('dotenv').config(); 
- const { google } = require('googleapis');
+import { Telegraf } from 'telegraf';
+import dotenv from 'dotenv'
+import { google } from 'googleapis';
 
-const { scrapeWebsite } = require('./src/scrapeWebsite')
-
+import { scrapeWebsite } from './src/scrapeWebsite.js'
+dotenv.config()
 // Use environment variables for security in a real project
 console.log('Starting bot with token:', process.env.BOT_TOKEN ? '***' : 'No token provided');
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -101,7 +100,7 @@ bot.command('mua', async (ctx) => {
     }
     try {
         // Ví dụ: thời gian, giá mua,số lượng
-        const values = [ new Date().toLocaleString().split(',')[0], parseInt(parts[1]), parseInt(parts[2])];
+        const values = [new Date().toLocaleString().split(',')[0], parseInt(parts[1]), parseInt(parts[2])];
         await appendToSheet(values, 'Mua!A:C');
         ctx.reply("✅ Tin nhắn của bạn đã được lưu vào Google Sheet!");
     } catch (err) {
@@ -111,7 +110,7 @@ bot.command('mua', async (ctx) => {
 });
 
 bot.command('giavang', async (ctx) => {
-    
+
     const targetUrl = 'https://ngocthinh-jewelry.vn/'; // Example target URL
     const giaVang = await scrapeWebsite(targetUrl);
     await updateToSheet([giaVang.giamuavao], 'Mua!F2:F3');
@@ -132,21 +131,21 @@ bot.on('text', (ctx) => {
 // Launch the bot
 // bot.launch();
 bot.launch({
-  webhook: {
-    // Public domain for webhook; e.g.: example.com
-    domain: webhookDomain,
+    webhook: {
+        // Public domain for webhook; e.g.: example.com
+        domain: webhookDomain,
 
-    // Port to listen on; e.g.: 8080
-    port: port,
+        // Port to listen on; e.g.: 8080
+        port: port,
 
-    // Optional path to listen for.
-    // `bot.secretPathComponent()` will be used by default
-    // path: webhookPath,
+        // Optional path to listen for.
+        // `bot.secretPathComponent()` will be used by default
+        // path: webhookPath,
 
-    // Optional secret to be sent back in a header for security.
-    // e.g.: `crypto.randomBytes(64).toString("hex")`
-    // secretToken: randomAlphaNumericString,
-  },
+        // Optional secret to be sent back in a header for security.
+        // e.g.: `crypto.randomBytes(64).toString("hex")`
+        // secretToken: randomAlphaNumericString,
+    },
 });
 // app.use(await bot.createWebhook({ domain: 'webhookDomain' }));
 
